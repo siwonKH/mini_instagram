d066b0bc418d0ae5aec308d7fdd3e84d66e9d269
siwonKH <kanghyunkim313@gmail.com>
2021-08-03 03:45:40 +0900

    urls.py added edit-url

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AddPost, Home, Login, LogOut, check
+from .views import AddPost, EditPost, Home, Login, LogOut, check
 
 app_name = 'insta'
 urlpatterns = [
     path('home', Home.as_view()),
     path('addpost', AddPost.as_view()),
+    path('editpost', EditPost.as_view()),
     path('login', Login.as_view()),
     path('logout', LogOut.as_view()),
     path('check', check)
