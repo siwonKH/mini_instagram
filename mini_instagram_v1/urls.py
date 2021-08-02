0479bca7e953bd5fc1b5e41e8bc125bfbc096f44
siwonKH <kanghyunkim313@gmail.com>
2021-08-02 21:21:55 +0900

    outer urls.py added protect-media-file feature

--- mini_instagram_v1/urls.py
+++ mini_instagram_v1/urls.py
@@ -2,10 +2,17 @@ from django.urls import path, include
 from insta.views import Home, PostsView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+# MEDIA 파일 보호
+def protected_file(request, path, document_root=None):
+    return redirect('/')
+
 
 urlpatterns = [
     path('', include('insta.urls')),
-    path('', PostsView.as_view(),  name="posts"),
+    path('', PostsView.as_view(), name="posts"),
     path('', Home.as_view()),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, protected_file, document_root=settings.MEDIA_ROOT)
