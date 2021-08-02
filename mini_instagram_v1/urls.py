6991ae938f6039af120433e80f7149aef7de340f
siwonKH <kanghyunkim313@gmail.com>
2021-08-03 01:38:40 +0900

    urls.py updated media file protect feature

--- mini_instagram_v1/urls.py
+++ mini_instagram_v1/urls.py
@@ -3,16 +3,20 @@ from insta.views import Home, PostsView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
+from django.views.static import serve
 
 
 # MEDIA 파일 보호
 def protected_file(request, path, document_root=None):
+    user_pk = request.session.get('user')
+    if user_pk:
+        return serve(request, path, document_root)
     return redirect('/')
 
 
 urlpatterns = [
     path('', include('insta.urls')),
     path('', PostsView.as_view(), name="posts"),
-    path('', Home.as_view()),
+    path('', Home.as_view()),  # this should be gone
 
 ] + static(settings.MEDIA_URL, protected_file, document_root=settings.MEDIA_ROOT)
