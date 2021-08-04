2eff31cd2c05db7bd8886e3a2f93e617981bc48a
siwonKH <kanghyunkim313@gmail.com>
2021-08-04 15:47:21 +0900

    urls.py add signup url

--- insta/urls.py
+++ insta/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddPost, EditPost, Home, Login, LogOut, MyPage
+from .views import AddPost, EditPost, Home, SignUp, Login, LogOut, MyPage
 from .utils import check
 
 app_name = 'insta'
@@ -7,6 +7,7 @@ urlpatterns = [
     path('home', Home.as_view()),
     path('addpost', AddPost.as_view()),
     path('editpost', EditPost.as_view()),
+    path('signup', SignUp.as_view()),
     path('login', Login.as_view()),
     path('logout', LogOut.as_view()),
     path('check', check),
