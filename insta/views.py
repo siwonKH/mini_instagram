0ac86c1193f282681a61aeb30abe8de56178e029
siwonKH <kanghyunkim313@gmail.com>
2021-08-06 00:49:38 +0900

    added comment feature on main page

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
-import random
 
+from django.core import serializers
 from django.http import HttpResponse
 from django.http.response import HttpResponseNotAllowed
 from django.shortcuts import render, redirect, get_object_or_404
@@ -9,7 +9,6 @@ from django import views
 from django.contrib.auth import logout
 from .models import User, Post, PostLike, PostComment
 from django.views.generic.list import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from .utils import get_random_unicode
 
 
@@ -112,18 +111,44 @@ class Comment(views.View):
         else:
             return redirect('/login')
 
-        post_id = request.POST['post_id']
-        comment = request.POST['comment-input']
+        post_id = str(request.POST['post_id'])
+        comment = request.POST['comment']
 
-        if post_id and comment and comment != "":
+        if post_id and post_id != "" and comment and comment != "":
             post_comment = PostComment()
-            post_comment.post_id = post_id
+            post_comment.post_id = get_object_or_404(Post, id=post_id)
             post_comment.user_id = user
             post_comment.comment = comment
             post_comment.save()
-            context = {'response': "success"}
+
+            comment_model = PostComment.objects.filter(post_id=post_id).order_by('created_at')[0:3]
+            user1 = ""
+            comment1 = ""
+            user2 = ""
+            comment2 = ""
+            user3 = ""
+            comment3 = ""
+            try:
+                user1 = comment_model[0].user_id.nickname
+                comment1 = comment_model[0].comment
+                user2 = comment_model[1].user_id.nickname
+                comment2 = comment_model[1].comment
+                user3 = comment_model[2].user_id.nickname
+                comment3 = comment_model[2].comment
+            except IndexError:
+                pass
+
+            context = {
+                'commentRes': "success",
+                'comment1': comment1,
+                'comment2': comment2,
+                'comment3': comment3,
+                'user1': user1,
+                'user2': user2,
+                'user3': user3
+            }
         else:
-            context = {'response': "fail"}
+            context = {'commentRes': "fail"}
         return HttpResponse(json.dumps(context), content_type="application/json")
 
     @staticmethod
@@ -213,27 +238,26 @@ class DeletePost(views.View):
 class Login(views.View):
     @staticmethod
     def post(request):
-        try:
-            email = request.POST['email']
-            password = request.POST['password']
-        except:
-            return redirect('/login')
+        email = request.POST['email']
+        password = request.POST['password']
+
         if User.objects.filter(email=email).exists():
             user = User.objects.get(email=email)  # 데이터베이스에서 user_id 에 해당하는 데이터 가져오기
             salted_password = str(user.salt) + str(password)  # 불러온 해당유저 데이터에서, 솔트값을 사용자 입력 값과 합침
             hashed_password = hashlib.sha256(salted_password.encode()).hexdigest()  # 솔트값과 합쳐진 사용자 입력값을 해시
 
             if str(user.password) == str(hashed_password):
+
                 request.session['user'] = user.id  # 로그인 성공!
                 request.session['profile_pic'] = user.profile_pic.url
-                context = {'msg': "success"}
+                context = {'loginRes': "success"}
                 return HttpResponse(json.dumps(context), content_type="application/json")
 
             else:
-                context = {'msg': "pw_fail"}  # 에러메시지 생성
+                context = {'loginRes': "fail"}  # 에러메시지 생성
                 return HttpResponse(json.dumps(context), content_type="application/json")
         else:
-            context = {'msg': "id_fail"}  # 에러메시지 생성
+            context = {'loginRes': "fail"}  # 에러메시지 생성
             return HttpResponse(json.dumps(context), content_type="application/json")
 
     @staticmethod
@@ -248,8 +272,9 @@ class Login(views.View):
 
 
 class LogOut(views.View):
-    def post(self, request):
-        pass
+    @staticmethod
+    def post(request):
+        return HttpResponseNotAllowed('GET')
 
     # 로그아웃
     @staticmethod
@@ -259,7 +284,8 @@ class LogOut(views.View):
 
 
 class SignUp(views.View):
-    def post(self, request):
+    @staticmethod
+    def post(request):
         name = request.POST['name']
         nickname = request.POST['nickname']
         email = request.POST['email']
@@ -271,14 +297,16 @@ class SignUp(views.View):
         user.nickname = nickname
         user.email = email
         if password_chk != password:
-            context = {'msg': "pw_fail"}
+            context = {'SignupRes': "pw_fail"}
             return HttpResponse(json.dumps(context), content_type="application/json")
         salt = get_random_unicode(10)
         user.salt = salt
         salted_password = str(salt) + str(password)
         user.password = hashlib.sha256(salted_password.encode()).hexdigest()
         user.save()
-        return redirect('/login')
+
+        context = {'SignupRes': "success"}
+        return HttpResponse(json.dumps(context), content_type="application/json")
 
     @staticmethod
     def get(request):
